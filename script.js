05055374e80096fb160174bf6a4a498a0f8459e0
SarangShende <142102130+Sarang2019@users.noreply.github.com>
2023-09-25 14:29:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+function change(){
+    const entered = document.getElementById("status");
+ 
+    let currentContent = getComputedStyle(entered).textContent;
+
+    if(currentContent == "Enter the Metaverse"){
+        entered.textContent = "entered ";
+    }
+    else {
+        entered.textContent = "Entered Metaverse", entered.style.fontSize = "30px";
+    }
+}
